bc3178383b615a883b3e97843e491962533396c7
John Poplett <john.poplett@acm.org>
2011-04-30 10:16:03 -0500

    Clean up.
    Removed experimental code. Eliminated warnings.A
    
    Signed-off-by: John Poplett <john.poplett@acm.org>

--- cl-shell.lisp
+++ cl-shell.lisp
@@ -40,22 +40,15 @@
 (defun standard-output-sink () 
   (sink #'(lambda (value) (format t "~A~%" value))))
 
+(defun make-channel () 
+  (make-instance 'channel))
+
+#|
+; Prototype of plumbing 
 (defun two-stage-pipe (producer consumer) 
   (let ((input-channel (make-instance 'channel)))
     (pexec (:name "producer") (funcall producer input-channel))
     (funcall consumer input-channel)))
-
-(defun make-channel () 
-  (make-instance 'channel))
-
-(defun count-files (directory &key (test (constantly t)))
-  (two-stage-pipe (find-files directory :test test) (message-count)))
-
-(defun count-files-with-suffix (directory suffix)
-  (count-files directory :test #'(lambda (file) (ends-with (namestring file) suffix))))
-
-(defun count-java-files (directory)
-  (count-files-with-suffix directory ".java"))
 
 (defun three-stage-pipe (producer filter consumer) 
   (let ((channel1 (make-instance 'channel))
@@ -63,33 +56,12 @@
     (pexec (:name "producer") (funcall producer channel1))
     (pexec (:name "filter") (funcall filter channel1 channel2))
     (funcall consumer channel2)))
+|#
 
 (defun pp-helper (args)
   (cond ((null args)
 	 '(standard-output-sink))
 	(t `(,@(car (last args))))))
-
-(defun make-pipes (producer &rest args)
-  (let ((consumer (pp-helper args))
-	(filters (butlast args)))
-    `(let ((channel1 (make-instance 'channel))
-	   (channel2 (make-instance 'channel)))
-       (pexec (:name "producer") (funcall producer channel1))
-       (pexec (:name "filter") (funcall filter channel1 channel2))
-       (funcall consumer channel2))))
-    
-(defmacro pp (producer &rest args)
-  (let ((input-channel (gensym))
-	(output-channel (gensym))
-	(producer-instance (gensym))
-	(consumer-instance (gensym)))
-    `(let* ((,input-channel (make-instance 'channel))
-	    (,output-channel ,input-channel)
-	    (,producer-instance ,producer)
-	    (,consumer-instance ,(pp-helper args)))
-       (pexec (:name "producer") (funcall ,producer-instance ,input-channel))
-       (funcall ,consumer-instance ,output-channel)
-       (format t "producer: ~A~%args: ~A~%" ,producer ,(pp-helper args)))))
 
 (defun mkstr (&rest args)
   (with-output-to-string (s)
@@ -98,35 +70,8 @@
 (defun symb (&rest args)
   (values (intern (apply #'mkstr args))))
 
-(defun symb-ref%% ()
-  (sym 'a 'b 'c))
-
 (defmacro channel-symbol (n)
   `(symb 'cl-shell::channel (write-to-string ,n)))
-
-(defun z () 
-  (let* ((path "/Users/john/Development/android/sdk/samples/android-9/JetBoy")
-	 (producer `(find-files ,path))
-	 (args '((null-filter) (message-count))))
-    `(let ((channel1 (make-instance 'channel))
-      ,@(loop for i from 2 to (+ 1 (length args))
-	     collect `(,(symb 'channel (write-to-string i)) (make-instance 'channel)))))))
-
-(defmacro p% (producer &rest args) 
-  (alexandria:with-gensyms (consumer filters)
-    `(let ((,consumer ,(pp-helper args))
-	   (,filters '(,@(butlast args)))
-	   (channel1 (make-instance 'channel))
-	   ,@(loop for i from 2 to (+ 1 (length args))
-		collect `(,(symb 'channel (write-to-string i)) (make-instance 'channel))))
-       (pexec (:name "producer") (funcall ,producer channel1))
-       ;(pexec (:name "filter") (funcall ,(car filters) channel1 channel2))
-       (funcall ,consumer channel2)
-       (format t "~A~%" ,filters))))
-
-(defun z% ()
- (let* ((path "/Users/john/Development/android/sdk/samples/android-9/JetBoy"))
-   (pprint (macroexpand-1 (p% (find-files path) (null-filter) (message-count))))))
 
 (defun filter-gen ()
   (let ((counter 1))
@@ -135,38 +80,26 @@
 	(setf counter (+ 1 counter))
 	`(pexec (:name "filter") (funcall ,filter ,(channel-symbol old-counter) ,(channel-symbol counter)))))))
 
-(defun p-helper%% (producer args consumer filters)
+(defun p-helper%% (producer args consumer)
     `(let ((,consumer ,(pp-helper args))
-	   ;(,filters '(,@(butlast args)))
 	   (channel1 (make-instance 'channel))
 	   ,@(loop for i from 2 to (length args)
 		collect `(,(channel-symbol i) (make-instance 'channel))))
        (pexec (:name "producer") (funcall ,producer channel1))
        ,@(mapcar (filter-gen) (butlast args))
-       ;(pexec (:name "filter") (funcall ,(car filters) channel1 channel2))
        (funcall ,consumer ,(channel-symbol (length args)))))
 
-(defmacro p%% (producer &rest args) 
-  (let ((consumer (gensym))
-	(filters (gensym)))
-    `(,@(p-helper%% producer args consumer filters))))
-
-(defun z%% ()
- (let* ((path "/Users/john/Development/android/sdk/samples/android-9/JetBoy"))
-   (p-helper%% `(find-files ,path) '((null-filter) (null-filter) (message-count)) (gensym) (gensym))
-   (pprint (macroexpand-1 (p%% (find-files path) (null-filter) (message-count))))))
-
-(defun z%%% ()
- (let* ((path "/Users/john/Development/android/sdk/samples/android-9/JetBoy"))
-   (p%% (find-files path) (null-filter) (null-filter) (message-count))))
-
 (defmacro -> (producer &rest args) 
-  (let ((consumer (gensym))
-	(filters (gensym)))
-    `(,@(p-helper%% producer args consumer filters))))
+  (let ((consumer (gensym)))
+    `(,@(p-helper%% producer args consumer))))
 
-
-(defun count-files% (directory &key (test (constantly t)))
+(defun count-files (directory &key (test (constantly t)))
   (-> (find-files directory :test test) (message-count)))
 
-(z%%%)
+(defun count-files-with-suffix (directory suffix)
+  (count-files directory :test #'(lambda (file) (ends-with (namestring file) suffix))))
+
+(defun count-java-files (directory)
+  (count-files-with-suffix directory ".java"))
+
+
